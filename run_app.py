51bdfa2947549da16cc684413ce468dcd550165c
witmul <83119547+witmul@users.noreply.github.com>
2022-04-08 22:05:39 +0200

    minor chages

--- run_app.py
+++ run_app.py
@@ -10,15 +10,17 @@ print(tabulate([["I want to install dependencies..."],
                headers=["Index", "Action"],
                tablefmt="pretty"))
 
-val = input("Please index of action: ")
+val = input("Please select index of action: ")
 
 if val == "0":
     #os.system("pip3 install -r requirements.txt")
     subprocess.call("pip3 install -r requirements.txt", shell=True)
-    input("Press ENTER to proceed")
+    input("Load completed. Press ENTER to proceed")
 elif val == "1":
     from db import save_in_db
 elif val == "2":
     subprocess.call(os.path.dirname(__file__) + "\\rrs\\app.py", shell=True)
+    input("Load completed. Press ENTER to proceed")
 else:
     print("Action index is not on the list!")
+    input("Press ENTER to proceed")

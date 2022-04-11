6df908820d57c5752f8c0d4eb50282776507c2fe
witmul <83119547+witmul@users.noreply.github.com>
2022-04-11 17:43:32 +0200

    minor chages and update of DB

--- run_app.py
+++ run_app.py
@@ -16,12 +16,14 @@ if val == "0":
     #os.system("pip3 install -r requirements.txt")
     subprocess.call("pip3 install -r requirements.txt", shell=True)
     input("Load completed. Press ENTER to proceed")
+
 elif val == "1":
     from db import save_in_db
-elif val == "2":
 
+elif val == "2":
     subprocess.call(os.path.dirname(__file__) + "\\db\\app.py", shell=True)
     input("Load completed. Press ENTER to proceed")
+
 else:
     print("Action index is not on the list!")
     input("Press ENTER to proceed")
